5a970c3801a1e62099f68e93520549b843a89d5f
ariane-s-f16 <arianedesouzaficho@gmail.com>
2025-08-21 15:57:07 -0300

    estou em enviar a mes e aparecer no html

--- chat-php/chat.php
+++ chat-php/chat.php
@@ -7,7 +7,23 @@
 </head>
 <body>
     <H2>CHAT </H2>
+    <!--receber as mensagens enviadas pelo js-->
     <span Id="mensagem_chat"></span>
     <script>
+        const mensagemChat = document.GetElementById("mensagem_chat");
+
+       const ws=  new Websocket('ws//localhost:')
+
+       ws onopen = (e) =>
+       {
+        console.log("conectado");
+       }
+       ws onmessage =(mensagemrecebida) =>
+       {
+       let resultado= JSON.parse(mensagemrecebida.data);
+
+       mensagemChat= insertAdjacentHTML('beforeend', '${resultado.mensagem}<br>');//falta coisa
+       }
+    </script>
 </body>
 </html>
