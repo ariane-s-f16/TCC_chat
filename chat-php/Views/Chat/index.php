d342988b6cb2e91c1d7c4e84e08cd06080dfecf8
ariane-s-f16 <arianedesouzaficho@gmail.com>
2025-09-01 15:58:34 -0300

    parei no minuto 48:11 do video manual do dev

---
+++ chat-php/Views/Chat/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css"> 
+    
+    <title>Document</title>
+</head>
+<body>
+    <section class="container">
+        <section class="chat">
+
+        <section class="chat_messages">
+            <div class="mensagem-self">hello. word</div>
+            <div class="mensagem-outra">
+            <span class="mensagem--sender">manual dev</span>
+            </div>
+           
+        </section>
+
+
+
+            <form class="chat_conversa">
+                <input type="Text" class="chat_input" placeholder="Digite uma mensagem" name="mensagem" required >
+                <button  type="submit" class="chat_button"> 
+                <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=send" />
+            </button>
+                
+            
+            </form>
+        </section>
+</section>    
+</body>
+</html>
