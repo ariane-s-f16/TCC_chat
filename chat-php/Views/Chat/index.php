250b62bd98f57e92ee5583a18e071989cdb775f6
ariane-s-f16 <arianedesouzaficho@gmail.com>
2025-09-04 15:20:51 -0300

    navbarfeito, entre muitas aspas

--- chat-php/Views/Chat/index.php
+++ chat-php/Views/Chat/index.php
@@ -5,31 +5,31 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css"> 
     
-    <title>Document</title>
+    <title>tela dividida</title>
 </head>
 <body>
-    <section class="container">
-        <section class="chat">
-
-        <section class="chat_messages">
-            <div class="mensagem-self">hello. word</div>
-            <div class="mensagem-outra">
-            <span class="mensagem--sender">manual dev</span>
-            </div>
-           
-        </section>
-
-
-
-            <form class="chat_conversa">
-                <input type="Text" class="chat_input" placeholder="Digite uma mensagem" name="mensagem" required >
-                <button  type="submit" class="chat_button"> 
-                <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=send" />
-            </button>
-                
-            
-            </form>
-        </section>
-</section>    
+<section class="chat-section">
+        <div class="chat-container">
+            <!-- start: Sidebar -->
+            <aside class="chat-sidebar">
+                <a href="#" class="chat-sidebar-logo">
+                    <i class="ri-chat-1-fill"></i>
+                </a>
+                <ul class="chat-sidebar-menu">
+                    <li class="active"><a href="#" data-title="Chats"><i class="ri-chat-3-line"></i></a></li>
+                    <li><a href="#" data-title="Contacts"><i class="ri-contacts-line"></i></a></li>
+                    <li><a href="#" data-title="Documents"><i class="ri-folder-line"></i></a></li>
+                    <li><a href="#" data-title="Settings"><i class="ri-settings-line"></i></a></li>
+                    <li class="chat-sidebar-profile">
+                        <button type="button" class="chat-sidebar-profile-toggle">
+                            <img src="https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8cGVvcGxlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" alt="">
+                        </button>
+                        <ul class="chat-sidebar-profile-dropdown">
+                            <li><a href="#"><i class="ri-user-line"></i> Profile</a></li>
+                            <li><a href="#"><i class="ri-logout-box-line"></i> Logout</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </aside>
 </body>
 </html>
